cee630e728f30c040b5351944cd465f431f7ec9c
danGRT <d.geraghty06@gmail.com>
2018-10-10 16:40:44 +0100

    exercises completed

--- src/index.js
+++ src/index.js
@@ -12,4 +12,88 @@ function increase(){
   return inner;
 }
 
-exports.increase = increase;
+function double() {
+  let number = 10;
+
+  function inner() {
+    return number*2;
+  }
+
+  return inner;
+}
+
+function total(entryNum){
+  let tot = entryNum
+
+  function inner(entryArray){
+    const arraySum = entryArray.reduce((acc,item) => acc + item)
+    tot += arraySum
+    return tot
+  }
+  return inner;
+}
+
+function gibberish() {
+  let myStr = '';
+  function inner(input) {
+    if (typeof input === 'string') {
+      myStr = myStr + ' ' + input;
+    }
+    else {
+      myStr = myStr + input.join(' ') + '.';
+    }
+    return myStr;
+  }
+  return inner;
+}
+
+function mergeSort(){
+  let array = []
+  function inner(input){
+    array = array.concat(input)
+    array.sort((a,b) => (b - a))
+    return array[0]
+  }
+  return inner
+}
+
+function calculator() {
+  let total = 0;
+  function inner(a,b,op) {
+    if (op === '+') {
+      total = total + a + b;
+    }
+    else if (op === '-') {
+      total = total + a - b;
+    }
+    else if (op === '*') {
+      total = total + a*b;
+    }
+    else {
+      total = total + a/b;
+    }
+    return total;
+  }
+  return inner;
+}
+
+
+function dogHome(){
+  let dogHomes = {
+    london: [],
+    paris: []
+  }
+
+  function houseDog(dog){
+    const location = dog.location
+    dogHomes[location] = dogHomes[location].concat(dog)
+
+  }
+  function getDogsByLocation(location){
+    return dogHomes[location]
+
+  }
+  return {houseDog, getDogsByLocation}
+}
+
+module.exports = {increase, double, total, gibberish, mergeSort,calculator, dogHome};
